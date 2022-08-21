57e2b14cbe0e27e1fb15d8e5c5c618da9b412112
Armageddon <armageddon292@mail.ru>
2022-08-21 13:06:48 +0600

    Создал программу которая сравнивает два числа

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число ");
+int numberA  = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.Write("число больше ");
+    Console.WriteLine(numberA);
+    Console.Write("число меньше ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("число больше ");
+    Console.WriteLine(numberB);
+    Console.Write("число меньше ");
+    Console.WriteLine(numberA);
+    }
