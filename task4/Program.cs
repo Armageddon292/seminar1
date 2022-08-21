933c9e59965a81d5ea8827d393f3347d2dcb98c4
Armageddon <armageddon292@mail.ru>
2022-08-21 13:09:04 +0600

    Создал программу которая находит максимальное число из трёх чисел

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число ");
+int numberA  = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int numberC  = int.Parse(Console.ReadLine());
+int max = numberA;
+
+if (numberB > max ) max = numberB;
+if (numberC > max ) max = numberC;
+
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
