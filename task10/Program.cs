920ad2d656e0c55df69c83ee6efa7e27dda96ed2
Armageddon <armageddon292@mail.ru>
2022-08-23 21:25:53 +0600

    Создал программу которая программу которая выводит вторую цифру из трёхзначного числа

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int randNum = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число: {randNum}");
+int twoNum = randNum / 10 % 10;
+Console.WriteLine(twoNum);
