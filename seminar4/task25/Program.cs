c05317cbf626c25a450f8fc17f486096d3a4e76b
Armageddon <armageddon292@mail.ru>
2022-08-31 21:47:48 +0600

    Создал программу которая возводит заданное число в степень второго заданного числа

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+Console.Write("Введите число А: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число В: ");
+int num2 = int.Parse(Console.ReadLine());
+int proizvedenie = 1;
+
+int VozvedenievStepen (int result)
+{
+    int num = 0;
+    int proizvedenie = 1;
+    while (num < num2)
+    {
+        proizvedenie = proizvedenie * num1;
+        num++;
+    }
+    return proizvedenie;
+}
+
+Console.WriteLine($"{VozvedenievStepen(proizvedenie)}");
