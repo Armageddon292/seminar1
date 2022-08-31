a16415a5313b39f2b29958018ce3267dccfe4f9c
Armageddon <armageddon292@mail.ru>
2022-08-31 21:07:25 +0600

    Создал программу которая задаёт массив из N элементов, заполненых случайными числами и выводит их на экран

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из N элементов, 
+// заполненных случайнми числами из [a, b) и выводит их на экран.
+
+Console.Write("Введите количество элементов массива: ");
+int Razmer = int.Parse(Console.ReadLine());
+
+int [] InitArray()
+{
+    int Length = Razmer;
+    int [] arr = new int[Length];
+    int i = 0;
+    while (i < arr.Length)
+    {
+        arr[i] = new Random().Next(1,6);
+        i++;
+    }
+    return arr;
+}
+
+Console.WriteLine(String.Join(", ", InitArray()));
