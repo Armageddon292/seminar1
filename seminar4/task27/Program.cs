eb15c512e0ea154e6f90883880824a3f0081428b
Armageddon <armageddon292@mail.ru>
2022-08-31 20:22:09 +0600

    Создал программу которая принимает число и выдает сумму всех цифр в числе

---
+++ seminar4/task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SummaChisel(int num)
+{
+    int sum = 0;
+    int col = 0;
+    int numsum =0;
+    while (num > 0)
+    {
+        numsum = num % 10;
+        sum = sum + numsum;
+        num = num / 10;
+        col++;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int summa = int.Parse(Console.ReadLine());
+Console.WriteLine($"{SummaChisel(summa)}");
