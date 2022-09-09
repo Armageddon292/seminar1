fffd9918c4776af20d6f3193f3ef1f54a6e621e9
Armageddon <armageddon292@mail.ru>
2022-09-10 03:13:10 +0600

    Задача41

---
+++ DZseminar6/task41/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество чисел: ");
+int numbers = int.Parse(Console.ReadLine());
+int[] myArray = new int[numbers];
+
+void VhodNumbers(int numbers)
+{
+    for (int i = 0; i < numbers; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+int ColPolNumbers(int[] myArray)
+{
+    int col = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] > 0)
+        {
+            col += 1;
+        }
+    }
+    return col;
+}
+
+VhodNumbers(numbers);
+
+Console.WriteLine($"Чисел больше 0: {ColPolNumbers(myArray)}");
