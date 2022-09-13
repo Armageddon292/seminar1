a3ee29d10e7daa33c28431b601ce10c3ec2c202d
Armageddon <armageddon292@mail.ru>
2022-09-13 11:41:25 +0600

    Задача52

---
+++ dzSeminar7/task52/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetArray(int m, int n,int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i=0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+     return result;   
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(m, n, 0, 10);
+PrintArray(myArray);
+
+for (int j = 0; j < myArray.GetLength(1); j++)
+{
+    double sredneeArif = 0;
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        sredneeArif = (sredneeArif + myArray[i,j]);
+    }
+    sredneeArif = sredneeArif / m;
+    Console.Write(sredneeArif + "; ");
+}
